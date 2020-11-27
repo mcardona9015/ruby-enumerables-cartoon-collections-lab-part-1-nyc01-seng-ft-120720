1e29d8e37a572e9d939d55c98b07b71f10430e12
Michael Cardona <mcardona9015@gmail.com>
2020-11-27 04:01:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |char, index|
-    puts "{index+1}. char"
+    puts "#{index+1}. char"
   end
 end
